11eb83d9d6b7f5d7dd0e1df8209512977c3fabe7
Matthew Baker <hackmattr@gmail.com>
2018-03-20 19:36:47 -0700

    Initial Commit

---
+++ Line Follow/Follow.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public enum MovementType
+    {
+        MoveTowards,
+        LerpTowards
+    }
+
+    public MovementType type = MovementType.MoveTowards;
+    public PathFollow MyPath;
+    public float speed = 1;
+    public float MaxDistanceToGoal = .1f;
+
+    private IEnumerator<Transform> pointsinPath;
+
+	// Use this for initialization
+	void Start () {
+		if (MyPath == null)
+        {
+            Debug.LogError("Need a path to follow");
+            return;
+        }
+
+        pointsinPath = MyPath.GetNextPathPoint();
+
+        pointsinPath.MoveNext();
+
+        if(pointsinPath.Current == null)
+        {
+            Debug.LogError("Need a point in the path", gameObject);
+            return;
+
+        }
+
+        transform.position = pointsinPath.Current.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pointsinPath == null || pointsinPath.Current == null)
+        {
+            return;
+        }
+        if (type == MovementType.MoveTowards)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pointsinPath.Current.position, Time.deltaTime * speed);
+        }
+        else if(type == MovementType.LerpTowards)
+        {
+            transform.position = Vector3.Lerp(transform.position, pointsinPath.Current.position, Time.deltaTime * speed);
+        }
+
+
+        var distanceSquared = (transform.position - pointsinPath.Current.position).sqrMagnitude;
+        if( distanceSquared < MaxDistanceToGoal * MaxDistanceToGoal)
+        {
+            pointsinPath.MoveNext();
+        }
+	}
+}
---
+++ Line Follow/PathFollow.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollow : MonoBehaviour {
+
+    public enum PathTypes
+    {
+        linear,
+        loop
+    }
+
+    public PathTypes Pathtype;
+    public int movementDirection = 1;
+    public int movingTo = 0;
+    public Transform[] PathSequence;
+
+    // Draw path lines in editor to show the path
+    public void OnDrawGizmos()
+    {
+        if(PathSequence == null || PathSequence.Length < 2)
+        {
+            return;
+        }
+        for(var i=1;i<PathSequence.Length; i++)
+        {
+            Gizmos.DrawLine(PathSequence[i - 1].position, PathSequence[i].position);
+        }
+
+        if(Pathtype == PathTypes.loop)
+        {
+            Gizmos.DrawLine(PathSequence[0].position, PathSequence[PathSequence.Length - 1].position);
+        }
+    }
+
+    public IEnumerator<Transform> GetNextPathPoint()
+    {
+        if (PathSequence == null || PathSequence.Length < 1)
+        {
+            yield break; // exit coroutine sequence length check fails.
+        }
+        while (true)
+        {
+            // return current point in pathsequence
+            yield return PathSequence[movingTo];
+
+            // if only one point exit coroutine.
+            if(PathSequence.Length == 1)
+            {
+                continue;
+            }
+            // if linear
+            if (Pathtype == PathTypes.linear)
+            {
+                if(movingTo <= 0)
+                {
+                    movementDirection = 1;
+                }else if(movingTo >= PathSequence.Length - 1)
+                {
+                    movementDirection = -1;
+                }
+            }
+            movingTo = movingTo + movementDirection;
+            if(Pathtype == PathTypes.loop)
+            {
+                if (movingTo >= PathSequence.Length)
+                {
+                    movingTo= 0;
+                }
+                if (movingTo < 0)
+                {
+                    movingTo = PathSequence.Length - 1;
+                }
+                }
+            }
+        }
+    }
